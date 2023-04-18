61a8b671dbe529e7e5421cb0f93158e8d749a4b4
rzamirzayev <rzatm@code.edu.az>
2023-04-18 11:45:59 +0400

    Add project files.

---
+++ dnTask1/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dnTask1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+    }
+}
---
+++ dnTask1/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dnTask1.Controllers
+{
+    public class ProductController:Controller
+    {
+        public ContentResult Detail(int id)
+        {
+            return Content(id.ToString());
+        }
+    }
+}
